4180b076903b308b1965c4b4e24219889136730f
Willington Vega <wvega@wvega.com>
2017-11-05 16:07:40 -0500

    Update reference to Photon source code.

--- inc/class-tachyon.php
+++ inc/class-tachyon.php
@@ -7,7 +7,7 @@ class Tachyon {
 	// Oh look, a singleton
 	private static $__instance = null;
 
-	// Allowed extensions must match http://code.trac.wordpress.org/browser/tachyon/index.php#L31
+	// Allowed extensions must match https://code.trac.wordpress.org/browser/photon/index.php#L33
 	protected static $extensions = array(
 		'gif',
 		'jpg',
