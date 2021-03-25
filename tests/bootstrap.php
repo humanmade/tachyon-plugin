4eca7b1d32882787a1bf2f214762616e4090fe59
Joe Hoyle <joehoyle@gmail.com>
2021-03-25 13:36:27 +0100

    Switch to using plugin-tester
    The test suite here is not working, like S3 uploads, I say we just switch to Plugin Tester

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,21 +3,15 @@
  * Bootstrap the plugin unit testing environment.
  *
  * @package WordPress
+ * @subpackage JSON API
 */
 
-// Support for:
-// 1. `WP_DEVELOP_DIR` environment variable
-// 2. Plugin installed inside of WordPress.org developer checkout
-// 3. Tests checked out to /tmp
-if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	$test_root = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
-} elseif ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
-	$test_root = '../../../../tests/phpunit';
-} elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
-	$test_root = '/tmp/wordpress-tests-lib';
-}
+require '/wp-phpunit/includes/functions.php';
 
-require $test_root . '/includes/functions.php';
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../tachyon.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Define a UR for Tachyon to use.
 define( 'TACHYON_URL', 'http://tachy.on/u' );
@@ -36,15 +30,9 @@ tests_add_filter( 'upload_dir', function( $upload_dir ) {
 	return $upload_dir;
 } );
 
-// Load Tachyon.
-tests_add_filter( 'muplugins_loaded', function() {
-	require_once dirname( __DIR__ ) . '/tachyon.php';
-} );
-
 // Always run Tachyon filters.
 tests_add_filter( 'tachyon_disable_in_admin', '__return_false' );
 tests_add_filter( 'tachyon_override_image_downsize', '__return_false', 999999 + 1 );
 
 
-
-require $test_root . '/includes/bootstrap.php';
+require '/wp-phpunit/includes/bootstrap.php';
